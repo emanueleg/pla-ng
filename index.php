a468445b7e56a22e8cba9ee88cef2c199c86401d
Emanuele Goldoni <emanuele.goldoni@gmail.com>
2024-01-13 11:23:34 +0100

    change version number to 1.9.9.1

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ if (is_readable($config_filename))
 
 //constants 1
 define("PROJECT", "phpLiteAdmin");
-define("VERSION", "1.9.9-dev");
+define("VERSION", "1.9.9.1");
 define("FORCETYPE", false); //force the extension that will be used (set to false in almost all circumstances except debugging, possible values: false, "PDO", "SQLite3", "SQLiteDatabase")
 define("SYSTEMPASSWORD", $password); // Makes things easier.
 define('PROJECT_URL','https://www.phpliteadmin.org/');
