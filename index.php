9148a298403770537d0d71143e92924a04cab8a9
Emanuele Goldoni <emanuele.goldoni@gmail.com>
2024-01-26 21:02:12 +0100

    Topnav title now links to the main page

--- index.php
+++ index.php
@@ -1625,7 +1625,7 @@ if(count($databases)==0) // the database array is empty, offer to create a new d
 //- HTML: sidebar
 echo '<table class="body_tbl" width="100%" border="0" cellspacing="0" cellpadding="0"><tr><td valign="top" class="left_td" style="width:100px; padding:9px 2px 9px 9px;">';
 echo "<div id='leftNav'>";
-echo "<h1><a href='".$params->getURL()."'>";
+echo "<h1><a href='".$_SERVER['PHP_SELF']."'>";
 echo "<span id='logo'>".PROJECT."</span> <span id='version'>v".VERSION."</span>";
 echo "</a></h1>";
 echo "<div id='headerlinks'>";
