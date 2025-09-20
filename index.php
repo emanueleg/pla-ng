c32c162c34bad7235beb2ede083752517f841d71
Emanuele Goldoni <emanuele.goldoni@gmail.com>
2025-09-20 18:36:26 +0200

    it's time for html5 - change the doctype

--- index.php
+++ index.php
@@ -1456,11 +1456,11 @@ ob_end_clean();
 //- HTML: output starts here
 header('Content-Type: text/html; charset=utf-8');
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $language; ?>" lang="<?php echo $language; ?>">
+<!DOCTYPE html>
+<html lang="<?php echo $language; ?>">
 <head>
 <!-- Copyright <?php echo date("Y").' '.PROJECT.' ('.PROJECT_URL.')'; ?> -->
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
+<meta charset="utf-8">
 <link rel="shortcut icon" href="?resource=favicon" />
 <title><?php echo PROJECT ?></title>
 
